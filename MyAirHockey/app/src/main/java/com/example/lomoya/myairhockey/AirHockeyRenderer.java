ec8d64905be5e7cdf5f5c001ece83ae40dcb36f3
liyan36 <liyan36@baidu.com>
2017-06-05 15:48:39 +0800

    feature:给顶点增加颜色和着色

--- MyAirHockey/app/src/main/java/com/example/lomoya/myairhockey/AirHockeyRenderer.java
+++ MyAirHockey/app/src/main/java/com/example/lomoya/myairhockey/AirHockeyRenderer.java
@@ -23,13 +23,18 @@ import javax.microedition.khronos.opengles.GL10;
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private static final String TAG = "AirHockeyRender";
 
-    private static final int POINT_COMPONENT_COUNT = 2;
-
     private static final int BYTES_PER_FLOAT = 4; // 一个float型占用4个字节
+
+    private static final int POINT_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE =
+            ((POINT_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT);
+
+
     private FloatBuffer vertexData;
 
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
+    private static final String A_COLOR = "a_Color";
+    private int aColorLocation;
 
     private static final String A_POSITION = "a_Position";
     private int aPositionLocation;
@@ -41,31 +46,41 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         mContext = context;
 
         float[] tableVerticesWithTriangles = new float[]{
-                -0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
+//                -0.5f, -0.5f,
+//                0.5f, 0.5f,
+//                -0.5f, 0.5f,
+//
+//                -0.5f, -0.5f,
+//                0.5f, -0.5f,
+//                0.5f, 0.5f,
+                // x, y, R, G, B
+                0f, 0f, 1.0f, 1.0f, 1.0f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                0f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
 
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
 
-                // outer border
-                -0.55f, -0.55f,
-                0.55f, 0.55f,
-                -0.55f, 0.55f,
+//                // outer border
+//                -0.55f, -0.55f,
+//                0.55f, 0.55f,
+//                -0.55f, 0.55f,
 
-                -0.55f, -0.55f,
-                0.55f, -0.55f,
-                0.55f, 0.55f,
+//                -0.55f, -0.55f,
+//                0.55f, -0.55f,
+//                0.55f, 0.55f,
                 // line
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 0f, 0f, 1f,
                 // pointer
-                0f, -0.25f,
-                0f, 0.25f,
+                0f, -0.25f, 0f, 0f, 1f,
+                0f, 0.25f, 0f, 0f, 1f,
 
-                // ball
-                0f, 0f
         };
 
         vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
@@ -91,13 +106,18 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         }
 
         GLES20.glUseProgram(programId);
-        uColorLocation = GLES20.glGetUniformLocation(programId, U_COLOR);
+        aColorLocation = GLES20.glGetAttribLocation(programId, A_COLOR);
         aPositionLocation = GLES20.glGetAttribLocation(programId, A_POSITION);
 
         vertexData.position(0);
         GLES20.glVertexAttribPointer(aPositionLocation, POINT_COMPONENT_COUNT,
-                GLES20.GL_FLOAT, false, 0, vertexData);
+                GLES20.GL_FLOAT, false, STRIDE, vertexData);
         GLES20.glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(2);
+        GLES20.glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT,
+                GLES20.GL_FLOAT, false, STRIDE, vertexData);
+        GLES20.glEnableVertexAttribArray(aColorLocation);
     }
 
     @Override
@@ -110,27 +130,18 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         Log.i(TAG, "---draw fram----");
         GLES20.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-        // draw outer border
-        GLES20.glUniform4f(uColorLocation, 0.5f, 1.0f, 1.0f, 1.0f);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 6, 6);
+//        // draw outer border
+//        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 6, 6);
 
         // draw inner border
-        GLES20.glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 10);
 
         // draw line
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-        GLES20.glDrawArrays(GLES20.GL_LINES, 12, 2);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 10, 2);
 
         // draw pointer
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 14, 1);
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 15, 1);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 12, 1);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 13, 1);
 
-        // draw ball
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        GLES20.glVertexAttribPointer();
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 16, 1);
     }
 }
